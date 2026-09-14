95d9452b78d738f9c594d1abf413f1cd3bc59611
Guilherme França <guilfranca27@gmail.com>
2026-09-13 23:09:08 -0300

    Aula 2 - 2. Rotas de post, patch e delete Finalizada

--- rotas/livro.js
+++ rotas/livro.js
@@ -8,5 +8,17 @@ router.get("/", (req, res) => {
     res.send("Retorno de todos os livros");
 });
 
+router.post("/", (req, res) => {
+    res.send("Você fez uma requisição POST");
+});
+
+router.patch("/", (req, res) => {
+    res.send("Você fez uma requisição PATCH");
+});
+
+router.delete("/", (req, res) => {
+    res.send("Você fez uma requisição DELETE");
+});
+
 // Exportando exte router para outros arquivos
 module.exports = router;
